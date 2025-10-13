c8f07a5fbe50e7cba8df2944f39dc9e518162138
mobiljani <egybemail@gmail.com>
2025-10-13 20:19:02 +0100

    Database

---
+++ cmd/database/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+func main() {
+	// Resolve the string address to a UDP address
+	udpAddr, err := net.ResolveUDPAddr("udp", ":8080")
+
+	db := make(map[string]string)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	conn, err := net.ListenUDP("udp", udpAddr)
+	fmt.Printf("Server started on %s\n", udpAddr.String())
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+
+	// Read from UDP listener in endless loop
+	for {
+		var buf [512]byte
+		_, addr, err := conn.ReadFromUDP(buf[0:])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		in := string(buf[0:])
+
+		fmt.Printf("Received '%s'\n", string(buf[0:]))
+
+		if strings.Contains(in, "=") {
+			key := strings.Split(in, "=")[0]
+			db[key] = strings.Replace(in, key, "", 1)
+		} else {
+			m := fmt.Sprintf("%s=%s", in, db[in])
+			conn.WriteToUDP([]byte(m), addr)
+
+			fmt.Printf("Sent '%s'\n", string(m))
+		}
+	}
+
+}

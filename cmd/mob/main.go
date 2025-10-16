e0695ed2699b8680ea89584c489d7ed48c7c46c9
mobiljani <egybemail@gmail.com>
2025-10-16 08:42:09 +0100

    Removing context usage and unused signal listeners

--- cmd/mob/main.go
+++ cmd/mob/main.go
@@ -2,25 +2,15 @@ package main
 
 import (
 	"bufio"
-	"context"
 	"fmt"
 	"net"
 	"regexp"
 	"strings"
-
-	"github.com/google/uuid"
-	"github.com/maniartech/signals"
 )
 
-type contextKey string
-
-var userMessaged = signals.New[string]()
-var serverMessaged = signals.New[string]()
-var key contextKey = "id"
 var tonysCoinAddr = "7YWHMfk9JZe0LM0g1ZauHuiSxhI"
 
 func main() {
-
 	list, err := net.Listen("tcp", ":8080")
 	fmt.Printf("Server started on port 8080\n")
 
@@ -28,8 +18,6 @@ func main() {
 		fmt.Printf("Error: %v\n", err)
 		return
 	}
-	cb := context.Background()
-
 	for {
 		conn, err := list.Accept()
 
@@ -37,23 +25,12 @@ func main() {
 			fmt.Printf("Error: %v\n", err)
 			return
 		}
-
-		ct := context.WithValue(cb, key, uuid.New().String())
-		ctx, cancel := context.WithCancel(ct)
-		go handleUserConnection(conn, ctx, cancel)
+		go handleUserConnection(conn)
 	}
 }
 
-func handleUserConnection(upstream net.Conn, ctx context.Context, cancel context.CancelFunc) {
-	defer cancel()
+func handleUserConnection(upstream net.Conn) {
 	defer upstream.Close()
-
-	// serverMessaged.AddListener(func(c context.Context, msg string) {
-	// 	if c.Value(key) == ctx.Value(key) {
-	// 		fmt.Printf("Downstream msg to user: '%s'\n", msg)
-	// 		upstream.Write([]byte(tonify(msg) + "\n"))
-	// 	}
-	// })
 
 	downstream, err := net.Dial("tcp", "chat.protohackers.com:16963")
 	fmt.Printf("Client started on port chat.protohackers.com:16963\n")
@@ -67,43 +44,24 @@ func handleUserConnection(upstream net.Conn, ctx context.Context, cancel context
 
 	scanner := bufio.NewScanner(upstream)
 	fmt.Printf("Starting to read user messages\n")
-	go handleServerConnection(upstream, downstream, ctx, cancel)
+	go handleServerConnection(upstream, downstream)
 
 	for scanner.Scan() {
 		in := scanner.Text()
 		fmt.Printf("user: '%s'\n", in)
-		//userMessaged.Emit(ctx, in)
 		downstream.Write([]byte(tonify(in) + "\n"))
-
-		if ctx.Err() != nil {
-			fmt.Printf("connection ctx has been cancelled\n")
-			break
-		}
 	}
 }
 
-func handleServerConnection(upstream net.Conn, downstream net.Conn, ctx context.Context, cancel context.CancelFunc) {
+func handleServerConnection(upstream net.Conn, downstream net.Conn) {
 	defer downstream.Close()
-	defer cancel()
-
-	// userMessaged.AddListener(func(c context.Context, msg string) {
-	// 	if c.Value(key) == ctx.Value(key) {
-	// 		fmt.Printf("Sending user msg to downstr: '%s'\n", msg)
-	// 		downstream.Write([]byte(tonify(msg) + "\n"))
-	// 	}
-	// })
+	defer upstream.Close()
 
 	scanner := bufio.NewScanner(downstream)
 	for scanner.Scan() {
 		in := scanner.Text()
 		fmt.Printf("server: '%s'\n", in)
-		// serverMessaged.Emit(ctx, in)
 		upstream.Write([]byte(tonify(in) + "\n"))
-
-		if ctx.Err() != nil {
-			fmt.Printf("downstream ctx has been cancelled\n")
-			break
-		}
 	}
 }
 
